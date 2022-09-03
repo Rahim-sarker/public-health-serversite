8cae9821b861aca2270c18fbd0444c124c4c5423
Rahim-sarker <mhrahim08@gmail.com>
2022-09-03 13:46:05 +0600

    Database integrate

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.roz9e8l.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+
+      await client.connect();
+    const bookingCollection = client.db("doctors_portal").collection("bookings");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+
+app.post('/booking',async(req,res)=>{
+  const booking = req.body;
+  const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+  const result = await bookingCollection.insertOne(booking);
+  res.send(result);
+})
+
+
+
+
+
+
+
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Public health service Server");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
