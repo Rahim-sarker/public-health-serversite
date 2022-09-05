b4066e6ede2b9be7543f1078857ef8c87a692041
Rahim-sarker <mhrahim08@gmail.com>
2022-09-06 00:52:57 +0600

    DashBoard done

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
   })
 
 
+  app.get('/booking', async(req,res)=>{
+    const patient = req.query.patient;
+    const query = {patient: patient}
+    const bookings = await bookingCollection.find(query).toArray();
+    res.send(bookings);
+  })
+
 
 
 
